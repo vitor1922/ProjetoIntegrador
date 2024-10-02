9828c8e4993f74ac58b647ad046a0e3ad8761b0e
vitor1922 <vitor.1922@aluno.pr.senac.br>
2024-10-02 19:50:57 -0300

    02/10/24 - 19h51

--- screens/popUpThalles.php
+++ screens/popUpThalles.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PopUp</title>
     <link rel="stylesheet"href="../src/bootstrap/css/bootstrap.min.css">
-    <link rel
+    <link rel="stylesheet
 </head>
 <body>
     
